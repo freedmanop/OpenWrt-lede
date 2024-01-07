88d6da08c1662f73e390d33b2ebee7790213a659
freedmanop <155171930+freedmanop@users.noreply.github.com>
2024-01-07 23:32:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git luci_app_xray https://github.com/yichya/luci-app-xray' >>feeds.conf.default
